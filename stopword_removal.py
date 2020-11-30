d80bc8174f11a0864f3b2ab4714f7c34f363f2df
jyoti9416 <64412794+jyoti9416@users.noreply.github.com>
2020-11-30 11:32:12 +0530

    Update stopword_removal.py

--- stopword_removal.py
+++ stopword_removal.py
@@ -1,6 +1,15 @@
 # write your code here to remove stopwords from the book review(book
 
-from nltk import word_tokenizer
+from nltk.tokenize import RegexpTokenizer
 nltk.download('stopwords')
+with open('book1.txt','r') as fp:
+  file = fp.read()
+words = RegexpTokenizer(file,'\w+')
+stop_words = set(stopwords.words('english'))
+no_stopwords = [word for word in words if not word in stop_words]
+with open('data.csv','w') as fp:
+  for i in no_stopwords:
+    fp.write(i)
+
 
 
